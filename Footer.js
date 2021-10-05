96bab211880e059f6e3e4130d25fe67a341a19d0
oliviaahlstrom <olah0007@ad.umu.se>
2021-10-05 10:15:55 +0200

    Detta är homepage-fix

--- Footer.js
+++ Footer.js
@@ -1,7 +1,29 @@
 import React from 'react';
 import { StyleSheet, Text, View, TouchableOpacity } from 'react-native';
 import * as RootNavigation from './RootNavigation';
+import { Icon } from 'react-native-vector-icons';
+
+import colors from './config/colors';
+
 
 export default function Footer() {
+    <View style={styles.footer}>
+        <TouchableOpacity style={styles.button} onPress={() => RootNavigation.navigate('CampusKaffe')}>
+            <Icon 
+            name='map-outline'
+            type='ionicon'/>
+        </TouchableOpacity>
+
+    </View>
 
 }
+
+const styles = StyleSheet.create({
+    footer: {
+        width: '100%',
+        height: 80,
+        flexDirection: 'row',
+        alignItems: 'flex-start',
+
+    }
+});
