d8b18d40451751356c13d13f8e9fab9ff061794a
id18nvk <id18nvk@cs.umu.se>
2021-10-05 11:55:43 +0200

    lägger till pin i karta

--- Map.js
+++ Map.js
@@ -1,22 +1,27 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
-import MapView, { PROVIDER_GOOGLE } from 'react-native-maps'
+import MapView, { PROVIDER_GOOGLE, Marker } from 'react-native-maps'
 
 
 export default function Map() {
     return (
       
-            <MapView
-            style={{ flex: 1 }}
-            provider={PROVIDER_GOOGLE}
-            showsUserLocation
-            showsMyLocationButton
-            initialRegion={{
-            latitude: 63.8195311,
-            longitude: 20.3077981,
-            latitudeDelta: 0.00015,
-            longitudeDelta: 0.0020}}
-            />
+        <MapView
+        style={{ flex: 1 }}
+        provider={PROVIDER_GOOGLE}
+        showsUserLocation
+        initialRegion={{
+        latitude: 63.8195311,
+        longitude: 20.3077981,
+        latitudeDelta: 0.00015,
+        longitudeDelta: 0.0020}}
+        >
+            <Marker 
+            coordinate={{ latitude: 63.8195311, longitude: 20.307981}} 
+            title={"gott kaffe"}>
+                
+            </Marker>
+        </MapView>
    
       
     );
