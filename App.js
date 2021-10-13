9f091fb3ed80fd7794f1e5013dc28972b7a9669c
1049 <id18jam@cs.umu.se>
2021-10-13 09:52:46 +0200

    fixat errors

--- App.js
+++ App.js
@@ -7,20 +7,17 @@ import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import Homepage from './Home';
 import Map from './Map';
 import ListView from './ListView';
-
-import { Platform, View } from 'react-native';
+import { MaterialIcons } from '@expo/vector-icons';
 import { AppLoading } from 'expo';
 import Header from './Header';
-
+import { Platform, useColorScheme, View, SafeAreaView, StyleSheet, Text} from 'react-native';
 //import Alla sidor
 import { navigationRef } from './RootNavigation';
-import { SafeAreaView, StyleSheet } from 'react-native';
-
 import colors from './config/colors';
 import PopUpShop from './PopUpShop';
 import PopUpFree from './PopUpFree';
 import CoffeeForm from './CoffeeForm';
-
+import { Colors } from 'react-native/Libraries/NewAppScreen';
 
 
 const Stack = createStackNavigator();
@@ -28,9 +25,31 @@ const Tab = createBottomTabNavigator();
 
 function Home() {
   return (
-    <Tab.Navigator>
-      <Tab.Screen name="Map" component={Map} />
-      <Tab.Screen name="PopUpShop" component={PopUpShop} />
+    <Tab.Navigator 
+      screenOptions={{ headerShown: false,  }} 
+      tabBarOptions={{
+        showLabel: false, 
+        activeTintColor: colors.primaryBeige,
+        activeBackgroundColor: colors.primaryBlue,
+        inactiveTintColor: colors.primaryBlue,
+        inactiveBackgroundColor: colors.primaryBeige}}>
+
+      <Tab.Screen name="Map" component={Map} options={{
+        tabBarIcon: ({focused}) =>(
+          <View>
+            <MaterialIcons name='map' size={30} style={{ color: focused ? colors.primaryBeige : colors.primaryBlue }} />
+          </View>
+        ),
+      }}/>
+
+      {/*OBS ÄNDRA TILL LISTA HÄR*/}
+      <Tab.Screen name="PopUpShop" component={PopUpShop} options={{
+        tabBarIcon: ({focused}) =>(
+          <View>
+            <MaterialIcons name='list' size={30} style={{ color: focused ? colors.primaryBeige : colors.primaryBlue }} />
+          </View>
+        ),
+      }}/>
     </Tab.Navigator>
   );
 }
@@ -49,7 +68,7 @@ export default function App() {
         
         <Stack.Screen 
           name="Campuskaffe"
-          component={CoffeeForm}
+          component={Home}
         options={{
           header: () => <Header headerDisplay="Campuskaffe" />
         }}
