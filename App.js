46e4d52cafac1796e2f7c5d3f1f48f6fb0e924f9
oliviaahlstrom <olah0007@ad.umu.se>
2021-10-13 10:02:49 +0200

    färgändring och listvy i footer

--- App.js
+++ App.js
@@ -29,24 +29,24 @@ function Home() {
       screenOptions={{ headerShown: false,  }} 
       tabBarOptions={{
         showLabel: false, 
-        activeTintColor: colors.primaryBeige,
+        activeTintColor: colors.secondaryBeige,
         activeBackgroundColor: colors.primaryBlue,
         inactiveTintColor: colors.primaryBlue,
-        inactiveBackgroundColor: colors.primaryBeige}}>
+        inactiveBackgroundColor: colors.secondaryBeige}}>
 
       <Tab.Screen name="Map" component={Map} options={{
         tabBarIcon: ({focused}) =>(
           <View>
-            <MaterialIcons name='map' size={30} style={{ color: focused ? colors.primaryBeige : colors.primaryBlue }} />
+            <MaterialIcons name='map' size={30} style={{ color: focused ? colors.secondaryBeige : colors.primaryBlue }} />
           </View>
         ),
       }}/>
 
       {/*OBS ÄNDRA TILL LISTA HÄR*/}
-      <Tab.Screen name="PopUpShop" component={PopUpShop} options={{
+      <Tab.Screen name="ListView" component={ListView} options={{
         tabBarIcon: ({focused}) =>(
           <View>
-            <MaterialIcons name='list' size={30} style={{ color: focused ? colors.primaryBeige : colors.primaryBlue }} />
+            <MaterialIcons name='list' size={30} style={{ color: focused ? colors.secondaryBeige : colors.primaryBlue }} />
           </View>
         ),
       }}/>
