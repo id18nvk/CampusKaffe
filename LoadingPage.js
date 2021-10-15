eb612a1be5c469d8b21b9a6426f14bc182caabab
1049 <id18jam@cs.umu.se>
2021-10-15 11:24:20 +0200

    lagt till text på loadingpage och stylat

--- LoadingPage.js
+++ LoadingPage.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import { Component } from "react";
 import { View, StyleSheet, Image } from 'react-native';
+import text from './assets/textloadingpage.png'
 
 export default class LoadingPage extends Component{
     render(){
         return(
             <View style={styles.container}>
+                <Image source={text} style={{width: 200, resizeMode: 'contain', marginBottom: 80 }}/>
                 <Image source={require('./assets/GifBack3.gif')}/>
             </View>
         );
@@ -18,5 +20,6 @@ const styles = StyleSheet.create({
         backgroundColor: '#3B555D',
         alignItems: 'center',
         justifyContent: 'center'
-    }
+    },
+   
 })
